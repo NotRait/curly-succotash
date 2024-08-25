6e96296e89beceef86637245853ac47650a6ac3e
NotRait <000webhostrati@gmail.com>
2024-08-25 21:43:04 +0400

    Made random string ascii only

--- random_string_gen.py
+++ random_string_gen.py
@@ -4,6 +4,8 @@ import string
 length = 10
 rand_string = ""
 
-for i in range(length) :  rand_string = rand_string + random.choice(string.printable)
+for i in range(length) :  
+    random_printable =  random.choice(string.ascii_letters)
+    rand_string = rand_string + random_printable
 
 print(rand_string)
